06eb919fb2470f760ccff6de32f3c9a1fdc72adb
Vitor Della <vitor.origamer@gmail.com>
2021-03-03 19:02:05 -0300

    added a1's folder and README.md

---
+++ a1/README.md
@@ -0,0 +1,31 @@
+Suguru is a game in which the player has to fill
+cells carefully placed inside a grid of changeble size.
+
+In the grid, there will be regions with N cells to
+be completed and, completing those, the player must
+use only numbers from 1 to N. (e. g. if the region has
+5 cells, the player must use 1, 2, 3, 4 and 5 to complete
+that said region).
+
+The only constraints given are that _the player cannot
+repeat the same exact number in adjacent cells, horizontally,
+vertically or diagonally._
+
+ex:
+
+2|2 4
+1|3 5 -> **Bad game**, as we have two 2's at the first row and
+4 5|1    two 5's next to one another at the bottom-right.
+
+>> So, how regions are made (or chosen)?
+Ramdonly. In order to solve that problem, as we are dealing with
+code, we must pass each region's coordinate inside the file to
+be parsed. 
+
+ex: 0 1
+    0 2
+    0 3     x x x
+    1 0  -> - - -  (2 x 3 grid)
+    1 1     y y y
+    1 2
+    1 3
--- act6/src/ex1.hs
+++ act6/src/ex1.hs
@@ -7,19 +7,25 @@ type Student = (Name, Subject, Grades)
 studentAt :: Int -> Student
 studentAt 0 = ("Jorge", "Matemática Discreta", [6.5, 7.2, 8.5])
 studentAt 1 = ("Camila", "Cálculo 4", [7.6, 8.5, 9.4])
+studentAt 2 = ("Romeu", "Fundamentos de Biologia Molecular", [0, 2, 3])
 
 nameOf :: Student -> Name
 nameOf (studentName, _, _) = studentName
 
-averageOf :: Student -> Float
-averageOf (_, _, grades) = reduce grades / fromIntegral (length grades)
+averageOf :: Int -> Float
+averageOf studentId = do
+    let student = studentAt studentId
+        gradesOf (_, _, grades) = grades
+        averageOf grades = sum' grades / fromIntegral (length grades)
+        in 
+        averageOf (gradesOf student)
 
-reduce :: [Float] -> Float
-reduce [] = 0
-reduce (a:b) = a + reduce b
+    where sum' (a:b) = a + sum b
+          sum' [] = 0 
 
 main = do
     let firstStudent = studentAt 0
     print firstStudent
     print (nameOf firstStudent)
-    print (averageOf firstStudent)
+    print (averageOf 0)
+    print (averageOf 2)

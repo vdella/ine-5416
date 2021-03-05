504c38ce3ee099734ff00d882519428ab6291107
Matheus Eyng de França <matheuseyngsf@gmail.com>
2021-03-05 16:37:42 -0300

    Renomeia Region para Row

--- a1/src/suguru.hs
+++ a1/src/suguru.hs
@@ -1,10 +1,10 @@
 import qualified Data.Char {-- Podemos usar isso? --}
 
 data Cell =  Initial Int | Possible [Int] deriving (Show, Eq) {-- Uma celula pode ja estar definida ou ser um cojunto de possiveis valores --}
-type Region = [Cell]
-type Board = [Region]
+type Row = [Cell]
+type Board = [Row]
 
-readBoard :: String -> Region -> Board -> Board {-- Itera sobre a string (arquivo lido) e adiciona cada coluna ao tabuleiro (Board) --}
+readBoard :: String -> Row -> Board -> Board {-- Itera sobre a string (arquivo lido) e adiciona cada coluna ao tabuleiro (Board) --}
 readBoard [] list board = board ++ [list] {-- Fim do arquivo, adiciona o a ultima linha lida --}
 readBoard (x:xs) list board {-- Nao temos mais o que ler, adiciona linha ao tabuleiro --}
   | x == '\n' = readBoard xs [] (board ++ [list]) {-- \n Marca que terminamos de ler uma linha, entao adicionamos ao tabuleiro, lemos a proxima --}

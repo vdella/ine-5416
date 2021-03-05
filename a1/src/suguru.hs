06394df9dfa3aa07e1f2d348850142b03b36bc26
Matheus Eyng de França <matheuseyngsf@gmail.com>
2021-03-05 17:38:05 -0300

    Adiciona novas funcoes

--- a1/src/suguru.hs
+++ a1/src/suguru.hs
@@ -12,9 +12,28 @@ readBoard (x:xs) list board {-- Nao temos mais o que ler, adiciona linha ao tabu
   | x == '0' = readBoard xs (list ++ [Possible [1..9]]) board {-- Marcacao de valor a ser completado, preenchemos com lista de possiveis valores --}
   | otherwise = readBoard xs (list ++ [Initial (Data.Char.digitToInt x)]) board {-- valor previamente fixo --}
   
+{-- dado o tabuleiro, indice e max de linhas, devolve as linhas adjacentes --}
+adjecentRows :: Board -> Int -> Int -> [Row]
+adjecentRows board i max | i == 0 = [board!!1]
+                         | i == max = [board!!(i-2)]
+                         | i >= max = []
+                         | otherwise = [board!!(i-2) ++ board!!i]
+
+{-- dado uma linha adjacente e o indice da celula original e o max de linhas, retorna as celulas adjacentes a essa celula (diagonais inclusas)--}
+adjacentCells :: Row -> Int -> Int -> [Cell]
+adjacentCells row i max | i == 0 = [row!!i] ++ [row!!(i+1)]
+                        | i == max = [row!!(i-1)] ++ [row!!i]
+                        | otherwise = [row!!(i-1)] ++ [row!!i] ++ [row!!(i+1)]
+
+contains :: [Int] -> Int -> Bool
+contains [] _ = False
+contains (x:xs) i | x == i = True 
+                  | otherwise = contains xs i
+
 
 main :: IO ()
 main = do
   s <- readFile "sudoku.txt"
-  let lista = readBoard s [] []
-  print . show $ lista
+  let board = readBoard s [] []
+  print . show $ board
+  print (show (adjecentRows board 2 3))

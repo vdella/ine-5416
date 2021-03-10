349c41f2c2f713d81b33b110bb268014a351cfcd
Vitor Della <vitor.origamer@gmail.com>
2021-03-09 22:11:17 -0300

    added Main module

--- a1/src/Main.hs
+++ a1/src/Main.hs
@@ -4,15 +4,23 @@ import qualified Data.Char
 import Types
 import Logic
 import Parsing
+import Data.Map (Map)
+import qualified Data.Map as Map
 
 main :: IO ()
 main = do
-  s <- readFile "sudoku.txt"
-  let board = readBoard s [] []
-  print . show $ board
-  putStrLn "Linhas adjacentes a linha 2"
-  print (show (adjecentRows board 2 3))
-  putStrLn "Initials"
-  print (show (getRegionInitials 1 board []))
-  putStrLn "Possibles reg 1"
-  print (show (getRegionPossibles 1 board []))
+  s <- readFile "suguru_board.txt"
+  -- print s
+  -- let board = readBoard' s 
+  -- print . show $ board
+  -- putStrLn "Linhas adjacentes a linha 2"
+  -- print (show (adjecentRows board 2 3))
+  -- putStrLn "Initials"
+  -- print (show (getRegionInitials 1 board []))
+  -- putStrLn "Possibles reg 1"
+  -- print (show (getRegionPossibles 1 board []))
+
+  -- print s
+  print (words s)
+  let newMap = readRegions' s
+  print newMap

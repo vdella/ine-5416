fd848fcb70032a6e849aa47d6bcf5c048714d6ad
Vitor Della <vitor.origamer@gmail.com>
2021-03-13 10:28:29 -0300

    removed non-used function

--- a1/src/Parsing.hs
+++ a1/src/Parsing.hs
@@ -29,30 +29,3 @@ printBoard' (x:xs) str = printBoard' xs ((printRow x str)++"\n")
 
 printBoard :: Board -> String 
 printBoard board = printBoard' board ""
-
--- {-- Reads the file in search of region strings to start its building.
---     As the return, gives a Map, which takes the region number (an Integer) as the key
---     and the region's coordinates as the value.
-
---     !!!FIXME: LAST REGION NOT PUT INSIDE THE MAP. ONLY GETS THE PREVIOUS REGIONS, NOT THE LAST.
--- --}
--- readRegions :: Int -> [Int] -> [String] -> Map Int [Int] -> Map Int [Int]
--- readRegions _ _ [] filledMap = filledMap  -- Being with empty [String], gives a new map back.
--- readRegions regionNumber elements (x : xs) mapToBeFilled =
---     if x == "region" then 
---       if regionNumber /= 0 then do
-
---         -- We have to insert the list of numbers of a given region inside the map right
---         -- at the last second: when we are about to start parsing some other new region!
-
---         let fillingMap = Map.insert regionNumber (tail elements) mapToBeFilled in  -- Use "tail elements" to evade using the proper region number
---           readRegions (regionNumber + 1) [] xs fillingMap                          -- such as "region 1". We want only what comes after that.
-
---       else readRegions (regionNumber + 1) [] xs mapToBeFilled  -- Increment region index and go on!
---     -- Append found number to list and parse the rest, then.
---     else readRegions regionNumber (elements ++ [actualNumber]) xs mapToBeFilled where actualNumber = read x :: Int
-
--- readRegions' :: String -> Map Int [Int]  -- Used during execution. Hides interns underneath.
--- readRegions' string' = readRegions 0 [] allElements filledMap
---   where filledMap = Map.empty
---         allElements = words string'

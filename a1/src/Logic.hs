9060c4e907c01f0c4ed2d369b63a0601e566af5f
Matheus Eyng de França <matheuseyngsf@gmail.com>
2021-03-20 10:27:06 -0300

    Muda funcao solve, porem loop infinito...

--- a1/src/Logic.hs
+++ a1/src/Logic.hs
@@ -189,10 +189,13 @@ getRegion' (Possible _ r) = r
 {-- tabuleiro, linha em que se comeca o solve, 0, 0, copia do tabuleiro--}                                                                                                            
 solveBoard :: Board -> Row -> Int -> Int -> Board -> Board
 solveBoard board [] _ _ _ = board
-solveBoard board ((Possible [] _):ys) i j alternativeBoard = solveBoard alternativeBoard (drop j (alternativeBoard!!i)) i j alternativeBoard -- caso nao haja mais opcoes para colocar no tabuleiro, voltamos para o estado anterior (possivelemnte errado esse segundo alternative board)
+solveBoard board ((Possible [] _):ys) i j alternativeBoard | j > 0 = solveBoard (updatedBoard i (j-1)) (drop (j-1) (updatedBoard i (j-1)!!i)) i (j-1) alternativeBoard -- caso nao haja mais opcoes para colocar no tabuleiro, voltamos para o estado anterior (possivelemnte errado esse segundo alternative board)
+                                                           | otherwise = solveBoard (updatedBoard (i-1) 0) (drop 0 (updatedBoard (i-1) 0!!(i-1))) (i-1) 0 alternativeBoard
+                                                            where
+                                                                updatedBoard x y = (insertCellBoard board (alternativeBoard!!x!!y) x y [])
 solveBoard board ((Possible (v:vs) r):ys) i j alternativeBoard | checkSafeInsert board v i j = if j < countLines board -1 -- checa se podemos inserir
-                                                                                                then solveBoard (insertCellBoard board (Initial v r) i j []) ys i (j+1) (insertCellBoard board (Possible vs r) i j []) -- inserimos e salvamos o board alternativo com os outros valores possiveis
-                                                                                                else solveBoard (insertCellBoard board (Initial v r) i j []) (primeiro (drop (i+1) board)) (i+1) 0 (insertCellBoard board (Possible vs r) i j []) -- mesma coisa mas pulamos a linha
+                                                                                                then solveBoard (insertCellBoard board (Initial v r) i j []) ys i (j+1) (insertCellBoard board (removeFromPossible (board!!i!!j) v) i j []) -- inserimos e salvamos o board alternativo com os outros valores possiveis
+                                                                                                else solveBoard (insertCellBoard board (Initial v r) i j []) (primeiro (drop (i+1) board)) (i+1) 0 (insertCellBoard board (removeFromPossible (board!!i!!j) v) i j []) -- mesma coisa mas pulamos a linha
                                                                | otherwise = solveBoard board ((Possible (vs) r):ys) i j alternativeBoard -- nao podemos inserir, tentamos com o proximo valor possivel
                                                                 where 
                                                                     primeiro xs | countLines xs == 0 = []
@@ -215,3 +218,6 @@ insertCellBoard :: Board -> Cell -> Int -> Int -> Board -> Board
 insertCellBoard [] _ _ _ newBoard = newBoard
 insertCellBoard (r:rs) cell x y newBoard | x == 0 = insertCellBoard rs cell (x-1) y (newBoard ++ [insertCellRow r cell y []])
                                          | otherwise = insertCellBoard rs cell (x-1) y (newBoard ++ [r])
+
+removeFromPossible :: Cell -> Value -> Cell
+removeFromPossible (Possible x r) val = Possible (filter (/= val) x) r

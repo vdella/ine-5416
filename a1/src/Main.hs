d1d1276ed09ffe401004232b02d9f37e39a5b95d
Vitor Della <vitor.origamer@gmail.com>
2021-03-22 16:20:59 -0300

    inserted backtracking functions in its own module
    	* Also cleaned part of the code and applied foldr
    	  where it was possible.

--- a1/src/Main.hs
+++ a1/src/Main.hs
@@ -7,25 +7,12 @@ import Parsing
 import Data.Map (Map)
 import qualified Data.Map as Map
 
+import Backtracking ( solve )
+
 main :: IO ()
 main = do
-  --s <- readFile "suguru_board.txt"
   s <- readFile "sudoku.txt"
-  let board = readBoard' s 
-  print (show board)
-  print (show (getUniqueRegions' board))
-  putStrLn "-----"
-  -- remove os valores impossiveis
+  let board = readBoard' s
   let newBoard = removeImpossiblesBoard (getUniqueRegions' board) board
-  putStrLn (printBoard newBoard)
-  -- remove valores ja presentes
   let trimmedBoard =  trimBoard (getUniqueRegions' newBoard) newBoard
-  putStrLn (printBoard trimmedBoard)
-
-  -- let solvedRow = solveRow board (board!!0) (adjacentRows board 0) 0 0 [] []
-  
-  -- print solvedRow
-  -- print (solveRow ([solvedRow] ++ (drop 1 trimmedBoard)) (trimmedBoard!!1) (adjacentRows trimmedBoard 1) 0 0 [] [])
-
-  -- print (solveBoard trimmedBoard)
-  putStrLn (printBoard (solveBoard trimmedBoard (trimmedBoard!!0) 0 0 trimmedBoard))
+  putStrLn (printBoard (solve trimmedBoard))

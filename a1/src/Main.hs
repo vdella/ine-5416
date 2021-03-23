793e0a24f22138913d28f48205e2b00b820b9603
Vitor Della <vitor.origamer@gmail.com>
2021-03-23 18:10:00 -0300

    adicionados comments

--- a1/src/Main.hs
+++ a1/src/Main.hs
@@ -10,12 +10,13 @@ import qualified Data.Map as Map
 main :: IO ()
 main = do
   s <- readFile "sudoku.txt"
+  {-- A primeira linha do arquivo-texto denota quantas linhas há na matriz. --}
   let max = Data.Char.digitToInt (head s)
-  let board = readBoard max (drop 1 s) []
+  let board = readBoard max (drop 1 s) []  -- Tira-se a linha 1 do arquivo-texto: passa-se somente a matriz.
   putStrLn "Tabuleiro original: "
-  putStrLn (printBoard board max)
+  putStrLn (printBoard board max)  -- Tabuleiro não alterado.
   let solvedBoard = solveBoard max board
   putStrLn "Tabuleiro resolvido: "
-  putStrLn (printBoard solvedBoard max)
+  putStrLn (printBoard solvedBoard max)  -- Tabuleiro resolvido.
 
   

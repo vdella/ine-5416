5b0a45181b6a9dc686f8f076a43043ed6a2dbdda
Vitor Della <vitor.origamer@gmail.com>
2021-03-31 16:59:39 -0300

    added inclusive range
    	* The inclusive-range function has to be used inside the scope of
    	  possibles-at. It will be used in order to gather all numbers
    	  that are possible inside a given region and, then, remove
    	  the ones seen at the adjacencies of a single cell;
    	* Minor notes include changes from camelCase to snake-case
    	  in order to work better alongside with Common Lisp's
    	  best practices;
    	* Renamed nList to reverse-range.

--- lisp/a2/src/logic.lisp
+++ lisp/a2/src/logic.lisp
@@ -18,132 +18,143 @@
 )
 
 ; Converte um índice da matriz para um ponto (i, j).
-(defun itopoint (i)
+(defun i-to-point (i)
   (make-point
     :i (floor (/ i 5))
     :j (mod i 5)))
 
 
 ; Converte um ponto para um índice da matriz.
-(defun pointoi (pnt)
-  (+ (point-j pnt) (* (point-i pnt) 5)))
+(defun point-to-i (pnt)
+  (+ (point-j pnt) (* (point-i pnt) 5))
+)
 
+; Retorna uma lista com os valores adjacentes ao índice i. Cnt serve como contador e deve comecar em 0
+(defun adj-row (i brd)
+  (setq actual (i-to-point i))
+  (setq row (point-i actual))
+
+  (cond
+    ((= 0 row) (in-row (+ row 1) brd 0))
+    ((= (- 5 1) row) (in-row (- row 1) brd 0))
+    (t (concat-lists (in-row (+ row 1) brd 0) (in-row (- row 1) brd 0)))
+  )
+)
 
 ; Retorna uma lista de valores na regiao dada
 ; r = regiao
 ; rgns = board de regioes
 ; brd = board
-(defun inRegion (r rgns brd)
+(defun in-region (r rgns brd)
   (cond
     ((null brd) '())  ; chegamos ao fim do board
-    ((= r (car rgns)) (cons (car brd) (inRegion r (cdr rgns) (cdr brd))))  ; estamos na regiao. Adicionamos na lista
-    (t (inRegion r (cdr rgns) (cdr brd)))))  ; caso contrario, pulamos para a proxima posicao
+    ((= r (car rgns)) (cons (car brd) (in-region r (cdr rgns) (cdr brd))))  ; estamos na regiao. Adicionamos na lista
+    (t (in-region r (cdr rgns) (cdr brd)))  ; caso contrario, pulamos para a proxima posicao
+  )
+)  
 
 
 ; Retorna o tamanho da regiao
-(defun regionLength (r)
-  (length (inRegion r regions board)))
+(defun region-length (r)
+  (length (in-region r regions board))
+)
 
 
 ; Remove da lista todas as ocorrencias de um dado valor
-(defun removeAll (val lista)
+(defun remove-all (val lista)
   (cond
     ((null lista) '())
-    ((= val (car lista)) (removeAll val (cdr lista)))
-    (t (cons (car lista) (removeAll val (cdr lista))))))
+    ((= val (car lista)) (remove-all val (cdr lista)))
+    (t (cons (car lista) (remove-all val (cdr lista))))
+  )
+)
 
 
 ; Remove da primeira lista os valores presentes na segunda lista
-(defun removeVal (lista1 lista2)
+(defun remove-val (lista1 lista2)
   (cond
     ((null lista1) '())
     ((null lista2) lista1)
-    (t (removeVal (removeAll (car lista2) lista1) (cdr lista2)))))
+    (t (remove-val (remove-all (car lista2) lista1) (cdr lista2)))
+  )
+)
 
 
-(defun possiblesAt (i brd)
+(defun possibles-at (i brd)
     (write-line (write-to-string i))
     (if (> i 24)
         ()  ; Retorna uma lista vazia, indicando index out of bounds.
         (if (/= (nth i brd) 0)
             (cons (nth i brd))
             (progn 
-                (setq max-length (nth i regions))
-                (setq to-be-filled ())
-                (setq to-remove (create-list-in-range to-be-filled 1 max-length))
+                (setq max (nth i regions))
+                (setq to-remove (inclusive-range max))
                 ; TODO needs to remove values from to-remove list.
-            )
-        )
-    )
-
-    (defun create-list-in-range (returnable lower upper)
-        (if (= lower upper)
-            returnable
-            (progn
-                (append returnable lower)  ; possible error.
-                (create-list-in-range returnable (+ 1 lower) upper)
+                ; TODO still needs helper functions.
             )
         )
     )
 )
 
+; Cria uma lista de 1 até 'max'. No caso, adicionar o valor 'min' é
+; opcional e este será interpretado como 1 por padrão. Itera
+; 1 número de cada vez (step).
+(defun inclusive-range (max &optional (min 1) (step 1))
+   (loop for n from min to max by step
+      collect n)
+)
 
-;; Retorna uma lista com os valores adjacentes ao indice i. Cnt serve como contador e deve comecar em 0
-(defun adjRow (i brd)
-  (setq actual (itopoint i))
-  (setq row (point-i actual))
-
-  (cond
-    ((= 0 row) (inrow (+ row 1) brd 0))
-    ((= (- 5 1) row) (inrow (- row 1) brd 0))
-    (t (concat-lists (inrow (+ row 1) brd 0) (inrow (- row 1) brd 0)))))
-
-
-
-;; Retorna a linha row, busca a partir de v (deve ser 0)
-(defun inrow (row brd v)
-  (setq actual (itopoint v))
+; Retorna a linha row, busca a partir de v (deve ser 0)
+(defun in-row (row brd v)
+  (setq actual (i-to-point v))
 
   (cond
     ((> (point-i actual) row) '())     ;; passamos do indice i
-    ((= (point-i actual) row) (cons (car brd) (inrow row (cdr brd) (+ 1 v))))
-    (t (inrow row (cdr brd) (+ 1 v))))) 
+    ((= (point-i actual) row) (cons (car brd) (in-row row (cdr brd) (+ 1 v))))
+    (t (in-row row (cdr brd) (+ 1 v)))
+  )
+) 
 
 (defun concat-lists (seq1 seq2)
   (if (null seq1)
       seq2
-      (cons (car seq1) (concat-lists (cdr seq1) seq2))))
+      (cons (car seq1) (concat-lists (cdr seq1) seq2)))
+)
 
 ; Retorna o indice da proxima celula vazia 
-(defun nextPossible (brd)
+(defun next-possible (brd)
   (if (= 0 (car brd))
     0
-    (+ 1 (nextPossible (cdr brd)))))
+    (+ 1 (next-possible (cdr brd)))
+  )
+)
 
 
 ; Cria uma lista nova com valores de 1 a n em reverso
-(defun nlist (n)
+(defun reverse-range (n)
   (if (<= n 0)
     '()
-    (cons n (nlist (- n 1)))))
+    (cons n (reverse-range (- n 1)))
+  )
+)
 
 (defun main()
     (write-line "Convertendo 7 para i e j")
-    (write-line (write-to-string (itopoint 11)))
-    (setq pnt (itopoint 14))
+    (write-line (write-to-string (i-to-point 11)))
+    (setq pnt (i-to-point 14))
     (write-line (write-to-string pnt))
-    (write-line (write-to-string (pointoi pnt)))
-    (write-line (write-to-string (inRegion 4 regions board)))
-    (write-line (write-to-string (regionLength 1)))
-    (write-line (write-to-string (removeAll 3 '(2 1 0 3 9 4 3))))
-    (write-line (write-to-string (removeVal '(2 1 0 3 9 4 3) '(2 3 9 3))))
-    (write-line (write-to-string (nextPossible board)))
-    (write-line (write-to-string (nlist 7))))
+    (write-line (write-to-string (point-to-i pnt)))
+    (write-line (write-to-string (in-region 4 regions board)))
+    (write-line (write-to-string (region-length 1)))
+    (write-line (write-to-string (remove-all 3 '(2 1 0 3 9 4 3))))
+    (write-line (write-to-string (remove-val '(2 1 0 3 9 4 3) '(2 3 9 3))))
+    (write-line (write-to-string (next-possible board)))
+    (write-line (write-to-string (reverse-range 7))))
     (write-line "AAAA")
     (write-line (write-to-string (region-board)))
-    (write-line (write-to-string (inrow 3 board 0)))
+    (write-line (write-to-string (in-row 3 board 0)))
     (write-line "linhas adjacentes")
-    (write-line (write-to-string (adjRow 7 board)))
+    (write-line (write-to-string (adj-row 7 board)))
     
 
 (main)
